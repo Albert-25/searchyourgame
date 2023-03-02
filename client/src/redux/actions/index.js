0cb272db25ac1ab216400c04b4d48a6b71512755
Albert-25 <ajst_95@outlook.com>
2023-03-02 12:20:29 -0500

    debugging

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export function getVideogames() {
     return async function (dispatch) {
-        var json = await axios.get("/videogames");
+        var json = await axios.get("searchyourgame-production.up.railway.app/videogames");
         console.log("function getVideogames()=>> ", json)
         console.log("json.data ", json.data)
         return dispatch({
